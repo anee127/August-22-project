9257ac01825cd221c1a55d8240bce7cdc1e86076
Anee <aneesa.khan97@outlook.com>
2022-08-11 20:39:25 +0000

    added sort menu

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-document .querySelector(".openChatBtn") .addEventListener("click", openForm);
+document.querySelector(".openChatBtn").addEventListener("click", openForm);
 document.querySelector(".close").addEventListener("click", closeForm);
 function openForm() {
    document.querySelector(".openChat").style.display = "block";
 }
 function closeForm() {
    document.querySelector(".openChat").style.display = "none";
+}
+
+function myFunction() {
+   document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+   if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+         var openDropdown = dropdowns[i];
+         if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+         }
+      }
+   }
 }
